2e98e4c54b376104418417a967f1ad9a67bc9c2f
Luu Nguyen Van <luu.nguyenvan2511@gmail.com>
2022-11-10 16:05:31 +0700

    upload code

---
+++ main.js
@@ -0,0 +1,14 @@
+const userInput = document.getElementById("userInput");
+let expression = '';
+const press=(num)=>{
+    expression+=num;
+    userInput.value = expression;
+}
+const equal=()=>{
+    userInput.value = eval(expression);
+    expression="";
+}
+const erase = ()=>{
+    expression = '';
+    userInput.value = expression;
+}
